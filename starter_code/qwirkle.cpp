56a5170c683d3ab1011fe5e5c8aaa80d0204e804
YiYang-Clotho <s3798354@student.rmit.edu.au>
2021-04-29 16:57:08 +0800

    add basic methods in main class, can be modify later for better efficiency

--- starter_code/qwirkle.cpp
+++ starter_code/qwirkle.cpp
@@ -1,15 +1,64 @@
-
 #include "LinkedList.h"
 
 #include <iostream>
+#include <string>
+using namespace std;
 
 #define EXIT_SUCCESS    0
 
 int main(void) {
    LinkedList* list = new LinkedList();
+
+   qwirkle();
+   int selection = getSelectionFromMenu();
+   if(selection == 1){
+      newGame();
+   }else if(selection == 2){
+      string fileName;
+      std::cout << "Enter the filename from which load a game" << std::endl;
+      std::cin >> fileName;
+
+      loadGame(fileName);
+   }else if(selection == 3){
+      credits();
+   }else if(selection == 4){
+      std::cout << "Goodbye" << std::endl;
+   }
+   
    delete list;
 
-   std::cout << "TODO: Implement Qwirkle!" << std::endl;
 
    return EXIT_SUCCESS;
 }
+
+void qwirkle(){
+   std::cout << "Welcome to Qwirkle!" << std::endl;
+   std::cout << "-------------------" << std::endl;
+}
+
+int getSelectionFromMenu(){
+   std::cout << "Menu" << std::endl;
+   std::cout << "----" << std::endl;
+   std::cout << "1. New Game" << std::endl;
+   std::cout << "2. Load Game" << std::endl;
+   std::cout << "3. Credits (Show student information)" << std::endl;
+   std::cout << "4. Quit" << std::endl;
+   
+   int input;
+   std::cin >> input;
+   return input;
+}
+
+void newGame(){
+
+}
+
+void loadGame(string fileName){
+   
+}
+
+void credits(){
+
+}
+
+
