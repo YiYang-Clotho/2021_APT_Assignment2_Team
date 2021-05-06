804dbd48125cab3710c1dcdfc78567f89ddd01a2
Sifan-Gao <s3753605@student.rmit.edu.au>
2021-05-07 00:10:37 +0800

    create save load Game code

---
+++ starter_code/save_load_Game.cpp
@@ -0,0 +1,10 @@
+#include "save_load_Game.h"
+
+bool saveGame(std::string savePath, Player * player1, Player * player2, TileBag * tileBag, Board * board) {
+
+	return false;
+}
+
+bool loadGame(std::string loadPath, Player * player1, Player * player2, TileBag * tileBag, Board * board) {
+	return false;
+}
---
+++ starter_code/save_load_Game.h
@@ -0,0 +1,12 @@
+#ifndef __SAVE_LOAD_GAME__
+#define __SAVE_LOAD_GAME__
+
+#include "Player.h"
+#include "Board.h"
+#include "TileBag.h"
+
+bool saveGame(std::string savePath, Player* player1, Player* player2, TileBag* tileBag, Board* board);
+
+bool loadGame(std::string loadPath, Player* player1, Player* player2, TileBag* tileBag, Board* board);
+
+#endif
