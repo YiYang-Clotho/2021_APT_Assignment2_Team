46c3ab08deb27fa5a83962b0b89adf0fc03b1e37
Sifan-Gao <s3753605@student.rmit.edu.au>
2021-05-11 00:26:09 +0800

    add isValidInstruction

--- starter_code/save_load_Game.h
+++ starter_code/save_load_Game.h
@@ -8,11 +8,23 @@
 #include <sstream>
 #include <fstream>
 
+const static int CMD_ILLEGAL=0;
+const static int CMD_PLACE_TILE=1;
+const static int CMD_REPLACE =2;
+const static int CMD_SAVE=3;
+const static int CMD_QUIT=4;
+
+
 
 // Save Game data to file 
 bool saveGame(std::string savePath, Player* player1, Player* player2, TileBag* tileBag, Board* board);
 
 // Load history Game data from file 
 bool loadGame(std::string loadPath, Player* player1, Player* player2, TileBag* tileBag, Board* board);
+
+// Verify the input command instruction
+int isValidInstruction(std::string inputInstruction,Player* player);
+
+
 
 #endif
